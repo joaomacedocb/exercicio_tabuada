37c4f5f5422b103d7ec2f226e92597b6962a36de
joaomacedocb <joaomacedocb@gmail.com>
2024-01-03 07:19:41 -0300

    subindo exercícios da semana: tabuada

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número até 10 para ver a tabuada:");
+        int numeroSelecionado = sc.nextInt();
+
+        while (numeroSelecionado <= 10) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(numeroSelecionado + " x " + i + " = " + numeroSelecionado * i);
+            }
+            numeroSelecionado++;
+        }
+
+
+
+        }
+    }
