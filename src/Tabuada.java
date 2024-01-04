01b33e7f8891336351065f976a7fb95eb2a48249
joaomacedocb <joaomacedocb@gmail.com>
2024-01-04 08:46:07 -0300

    Ajustado nome de arquivos e nome da classe

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Tabuada {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -13,8 +13,5 @@ public class Main {
             }
             numeroSelecionado++;
         }
-
-
-
-        }
     }
+}
