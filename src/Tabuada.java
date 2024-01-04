cab4ca36cb24befb001fd7bf219891e565864ac0
joaomacedocb <joaomacedocb@gmail.com>
2024-01-04 09:18:40 -0300

    Criação de sistema de opcoes para calculos desejados pelo usuario

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -4,14 +4,27 @@ public class Tabuada {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um número até 10 para ver a tabuada:");
-        int numeroSelecionado = sc.nextInt();
+        System.out.println("Boas-vindas ao programa Tabuada! Digite o número da opção desejada:");
+        System.out.println("1 - Ver tabuada completa de um número");
+        System.out.println("2 - Fazer uma multiplicação entre dois números");
+        System.out.println("3 - Ver listagem de tabuadas");
 
-        while (numeroSelecionado <= 10) {
-            for (int i = 1; i <= 10; i++) {
-                System.out.println(numeroSelecionado + " x " + i + " = " + numeroSelecionado * i);
-            }
-            numeroSelecionado++;
+        int opcaoSelecionada = sc.nextInt();
+
+        switch (opcaoSelecionada) {
+            case 1: System.out.println("Implementar...");
+            break;
+            case 2: System.out.println("Implementar 2...");
+            break;
+            case 3: int numeroInicial = 1;
+
+                    while (numeroInicial <= 10) {
+                        System.out.println("TABUADA DO NÚMERO " + numeroInicial + ":");
+                        for (int i = 1; i <= 10; i++) {
+                            System.out.println(numeroInicial + " x " + i + " = " + numeroInicial * i);
+                        }
+                        numeroInicial++;
+                    }
         }
     }
 }
